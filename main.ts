57e28db8201d92f74704cd1cc03ae3de0ef732b5
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-07-02 04:05:08 +0900

    create

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=70 color=#1eb0f0 icon="\u3041" block="i2c"
+namespace i2c {
+
+    //% shim=i2c::test
+    function test(n: number): number {
+        return 0;
+    }
+    /**
+      * test i2c device
+      * @param ad i2c address, eg: 0x32
+      */
+    //% blockId=test_i2c_device block="test i2c device %ad"
+    export function testi2c(ad: number):number {
+        return(test(ad));
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,1 @@
-// tests go here; this will not be compiled when this package is used as a library
+basic.showNumber(i2c.testi2c(0x32))
