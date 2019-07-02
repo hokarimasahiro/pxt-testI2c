1bae4e69d838a3a05c9a4717af8e9c9b0eab99fa
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-07-03 05:11:00 +0900

    update

--- main.ts
+++ main.ts
@@ -1,16 +1,28 @@
 //% weight=70 color=#1eb0f0 icon="\u3041" block="i2c"
 namespace testi2c {
 
-    //% shim=testi2c::testi2cc
-    function testi2cc(n: number): number {
+    //% shim=testi2c::testi2cr
+    function testi2cr(n: number): number {
+        return 0;
+    }
+    //% shim=testi2c::testi2cw
+    function testi2cw(n: number): number {
         return 0;
     }
     /**
-      * test i2c device
+      * test read i2c device
       * @param ad i2c address, eg: 0x32
       */
-    //% blockId=test_i2c_device block="test i2c device %ad"
-    export function testi2c(ad: number): number {
-        return (testi2cc(ad));
+    //% blockId=test_read_i2c_device block="test read i2c device %ad"
+    export function testReadI2c(ad: number): number {
+        return (testi2cr(ad));
+    }
+    /**
+      * test write i2c device
+      * @param ad i2c address, eg: 0x32
+      */
+    //% blockId=test_write_i2c_device block="test write i2c device %ad"
+    export function testWriteI2c(ad: number): number {
+        return (testi2cw(ad));
     }
 }
--- testi2c.cpp
+++ testi2c.cpp
@@ -2,9 +2,15 @@
 
 namespace testi2c {
 //%
-  uint32_t testi2cc(int32_t ad)
+  uint32_t testi2cr(int32_t ad)
   {
 	char buf[2];
     return uBit.i2c.read(ad << 1,buf,0,false);
+  }
+//%
+  uint32_t testi2cw(int32_t ad)
+  {
+	char buf[2];
+    return uBit.i2c.write(ad << 1,(const char *)buf,0,false);
   }
 }
