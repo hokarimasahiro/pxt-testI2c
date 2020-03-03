d059cdc2bc7f3e0d1ead30e66545261d3eee9385
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-03-04 05:32:51 +0900

    Updates.

--- testi2c.cpp
+++ testi2c.cpp
@@ -11,6 +11,6 @@ namespace testi2c {
   uint32_t testi2cw(int32_t ad)
   {
 	char buf[2];
-    return uBit.i2c.write(ad << 1,(const char *)buf,0,false);
+    return uBit.i2c.read((ad << 1) + 1,buf,0,false);
   }
 }
