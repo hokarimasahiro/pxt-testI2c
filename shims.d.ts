8d59980aa19ca87213b9609c14cf2a992706396b
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-12-03 06:11:37 +0900

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -1,23 +1,17 @@
-// Auto-generated. Do not edit.
-
-
-
     //% weight=70 color=#1eb0f0 icon="\uf0ad" block="i2c"
-declare namespace testi2c {
+namespace testi2c {
 
     /**
      * test read i2c device
      * @param ad i2c address, eg: 0x32
      */
     //% blockId=test_read_i2c_device block="test read i2c device %ad" shim=testi2c::testRead
-    function testRead(ad: int32): uint32;
+    export function testRead(ad: int32): uint32;
 
     /**
      * test write i2c device
      * @param ad i2c address, eg: 0x32
      */
     //% blockId=test_write_i2c_device block="test write i2c device %ad" shim=testi2c::testWrite
-    function testWrite(ad: int32): uint32;
+    export function testWrite(ad: int32): uint32;
 }
-
-// Auto-generated. Do not edit. Really.
