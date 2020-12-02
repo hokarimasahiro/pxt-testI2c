87baa8004b791ffe06cd525e73af0f5150cf50ab
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-12-03 07:55:52 +0900

    Update testi2c.cpp

--- testi2c.cpp
+++ testi2c.cpp
@@ -1,25 +1,16 @@
 #include "pxt.h"
 
-//% weight=70 color=#1eb0f0 icon="\uf0ad" block="i2c"
 namespace testi2c {
-    /**
-      * test read i2c device
-      * @param ad i2c address, eg: 0x32
-      */
-    //% blockId=test_read_i2c_device block="test read i2c device %ad"
-  uint32_t testRead(int32_t ad)
+//%
+  uint32_t testi2cr(int32_t ad)
   {
 	char buf[2];
-    return uBit.i2c.read((ad << 1) + 1,buf,0,false);
+    return uBit.i2c.read(ad << 1,buf,0,false);
   }
-    /**
-      * test write i2c device
-      * @param ad i2c address, eg: 0x32
-      */
-    //% blockId=test_write_i2c_device block="test write i2c device %ad"
-  uint32_t testWrite(int32_t ad)
+//%
+  uint32_t testi2cw(int32_t ad)
   {
 	char buf[2];
-    return uBit.i2c.read((ad << 1) + 0,buf,0,false);
+    return uBit.i2c.write(ad << 1,(const char *)buf,0,false);
   }
 }
