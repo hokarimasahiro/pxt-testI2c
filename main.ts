a0e17f13e2f746c33c1bed971b6f7d2f0ebf4f5c
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-12-06 01:38:29 +0900

    Update main.ts, testi2c.cpp, shims.d.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,12 @@
 //% weight=70 color=#1eb0f0 icon="\uf0ad" block="i2c"
 namespace testi2c {
 
-    //% shim=testi2c::testi2cr
-    function testi2cr(n: number): number {
-        return -1;
-    }
-    //% shim=testi2c::testi2cw
-    function testi2cw(n: number): number {
-        return -1;
-    }
     /**
       * test read i2c device
       * @param ad i2c address, eg: 0x32
       */
     //% blockId=test_read_i2c_device block="test read i2c device %ad"
+    //% shim=testi2c::testi2cr
     export function testRead(ad: number): number {
         return (testi2cr(ad));
     }
@@ -22,6 +15,7 @@ namespace testi2c {
       * @param ad i2c address, eg: 0x32
       */
     //% blockId=test_write_i2c_device block="test write i2c device %ad"
+    //% shim=testi2c::testi2cw
     export function testWrite(ad: number): number {
         return (testi2cw(ad));
     }
