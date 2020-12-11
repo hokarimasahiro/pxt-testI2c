7beb982a80f632c6a45a472019cfacca699effd3
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-12-12 07:51:56 +0900

    Update pxt.json, test.ts

--- test.ts
+++ test.ts
@@ -1,3 +1,19 @@
+function 上位表示 (HAD: number) {
+    L = 0
+    basic.clearScreen()
+    if (logic.bittest(HAD, 0)) {
+        led.plot(4, 0)
+    }
+    if (logic.bittest(HAD, 1)) {
+        led.plot(3, 0)
+    }
+    if (logic.bittest(HAD, 2)) {
+        led.plot(2, 0)
+    }
+    if (RW == 1) {
+        led.plot(0, 0)
+    }
+}
 input.onButtonPressed(Button.A, function () {
     H += -1
     if (H < 0) {
@@ -5,25 +21,15 @@ input.onButtonPressed(Button.A, function () {
     }
     上位表示(H)
 })
+input.onButtonPressed(Button.AB, function () {
+    RW = 1 - RW
+    上位表示(H)
+})
 input.onButtonPressed(Button.B, function () {
     H += 1
     if (H > 7) {
         H = 0
     }
-    上位表示(H)
-})
-function 上位表示(HAD: number) {
-    L = 0
-    basic.clearScreen()
-    if (HAD % 2 == 1) led.plot(4, 0)
-    if (Math.trunc(HAD / 2) % 2 == 1) led.plot(3, 0)
-    if (Math.trunc(HAD / 4) % 2 == 1) led.plot(2, 0)
-    if (RW == 1) {
-        led.plot(0, 0)
-    }
-}
-input.onButtonPressed(Button.AB, function () {
-    RW = 1 - RW
     上位表示(H)
 })
 let RC = 0
@@ -37,8 +43,8 @@ L = 8
 RW = 0
 上位表示(H)
 basic.forever(function () {
-    Y = Math.trunc(L / 4) + 1
-    X = L % 4 + 1
+    Y = logic.rshift(L, 2) + 1
+    X = logic.and(L, 3) + 1
     led.unplot(X, Y)
     if (H == 0 && L <= 0 || H == 7 && L >= 15) {
         RC = -1
