71fc17309b9bacd3d495edb1624e774526919ca9
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-12-12 07:18:23 +0900

    Update testi2c.cpp, shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -5,13 +5,6 @@ declare namespace testi2c {
      * test read i2c device
      * @param ad i2c address, eg: 0x32
      */
-    //% blockId=test_read_i2c_device block="test read i2c device %ad" shim=testi2c::testi2cr
-    function testi2cr(ad: int32): uint32;
-
-    /**
-     * test read i2c device
-     * @param ad i2c address, eg: 0x32
-     */
     //% blockId=test_write_i2c_device block="test write i2c device %ad" shim=testi2c::testi2cw
     function testi2cw(ad: int32): uint32;
 }
--- testi2c.cpp
+++ testi2c.cpp
@@ -1,24 +1,13 @@
 #include "pxt.h"
-
 namespace testi2c {
-  /**
-    * test read i2c device
-    * @param ad i2c address, eg: 0x32
-    */
-    //% blockId=test_read_i2c_device block="test read i2c device %ad"
   uint32_t testi2cr(int32_t ad)
   {
-    char buf[2];
-    return uBit.i2c.read(ad << 1,buf,0,false);
+      Buffer buf = createBuffer(2);
+      return uBit.i2c.read(address << 1, (BUFFER_TYPE)buf->data, 0, false);
   }
-  /**
-    * test read i2c device
-    * @param ad i2c address, eg: 0x32
-    */
-    //% blockId=test_write_i2c_device block="test write i2c device %ad"
   uint32_t testi2cw(int32_t ad)
   {
-    char buf[2];
-    return uBit.i2c.write(ad << 1,(const char *)buf,0,false);
+      Buffer buf = createBuffer(2);
+      return uBit.i2c.write(address << 1, (BUFFER_TYPE)buf->data, 0, false);
   }
 }
