7e9693fed60c5f1e4eb9e1e974c5592697e9ab60
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-12-14 03:44:41 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,7 +8,9 @@ namespace testi2c {
     //% blockId=test_read_i2c_device block="test read i2c device %ad"
     //% shim=testi2c::testi2cr
     export function testRead(ad: number): number {
-        return (-1);
+        basic.pause(100);
+        if(ad>>4==(ad & 0xf)) return (0);
+        else return (-1);
     }
     /**
       * test write i2c device
@@ -17,6 +19,8 @@ namespace testi2c {
     //% blockId=test_write_i2c_device block="test write i2c device %ad"
     //% shim=testi2c::testi2cw
     export function testWrite(ad: number): number {
-        return (-1);
+        basic.pause(50);
+        if(ad>>4==(ad & 0xf)) return (0);
+        else return (-1);
     }
 }
