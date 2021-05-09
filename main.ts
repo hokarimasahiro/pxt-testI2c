65250707a50a1a56c400e3b3482b4472245500bc
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2021-05-09 09:05:45 +0900

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -6,21 +6,23 @@ namespace testi2c {
       * @param ad i2c address, eg: 0x32
       */
     //% blockId=test_read_i2c_device block="test read i2c device %ad"
-    //% shim=testi2c::testi2cr
     export function testRead(ad: number): number {
-        basic.pause(100);
-        if(ad>>4==(ad & 0xf)) return (0);
-        else return (-1);
+        let buf=pins.i2cReadBuffer(ad, 8)
+        for (let i=0;i<8;i++){
+            if(buf[i]>0) return (0);
+        }
+        return (-1);
     }
     /**
       * test write i2c device
       * @param ad i2c address, eg: 0x32
       */
     //% blockId=test_write_i2c_device block="test write i2c device %ad"
-    //% shim=testi2c::testi2cw
     export function testWrite(ad: number): number {
-        basic.pause(50);
-        if(ad>>4==((ad & 0xf)-2)) return (0);
-        else return (-1);
+        let buf=pins.i2cReadBuffer(ad, 8)
+        for (let i=0;i<8;i++){
+            if(buf[i]>0) return (0);
+        }
+        return (-1);
     }
 }
